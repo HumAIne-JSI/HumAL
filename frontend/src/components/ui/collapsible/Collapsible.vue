92b56e877443a9f0b91631051bc478290482d009
fpdo <fpdo@gft.com>
2026-01-29 14:44:12 +0100

    Enhance Collapsible component to emit update events

--- frontend/src/components/ui/collapsible/Collapsible.vue
+++ frontend/src/components/ui/collapsible/Collapsible.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import { CollapsibleRoot, type CollapsibleRootProps } from 'radix-vue'
+import { CollapsibleRoot, type CollapsibleRootEmits, type CollapsibleRootProps } from 'radix-vue'
 
 const props = defineProps<CollapsibleRootProps>()
+const emit = defineEmits<CollapsibleRootEmits>()
 </script>
 
 <template>
-  <CollapsibleRoot v-bind="props">
+  <CollapsibleRoot v-bind="props" @update:open="emit('update:open', $event)">
     <slot />
   </CollapsibleRoot>
 </template>
